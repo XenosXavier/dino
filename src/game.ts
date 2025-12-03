293fbb826450f1c025fa9a5ccc56dccea536a4f7
xenos <xenosnfsg@gmail.com>
2025-12-04 02:36:38 +0800

    feat: Add game to handle game loop

---
+++ src/game.ts
@@ -0,0 +1,52 @@
+import { Canvas } from "@display";
+import { SceneManager } from "@manager";
+import { IdleScene, LoadScene, OverScene, PlayScene } from "@scene";
+import { CollisionSystem, InputSystem, RenderSystem } from "@system";
+
+export enum SceneName {
+  Load = "load",
+  Idle = "idle",
+  Play = "play",
+  Over = "over",
+}
+
+export default class Game {
+  public readonly canvas: Canvas;
+  public readonly inputSystem: InputSystem;
+  public readonly renderSystem: RenderSystem;
+  public readonly collisionSystem: CollisionSystem;
+  public readonly sceneManager: SceneManager<SceneName>;
+  private time;
+
+  public constructor(canvas: HTMLCanvasElement, width: number, height: number) {
+    this.canvas = new Canvas(canvas, width, height);
+    this.inputSystem = new InputSystem();
+    this.renderSystem = new RenderSystem();
+    this.collisionSystem = new CollisionSystem();
+    this.sceneManager = this.createSceneManager();
+    this.time = 0;
+  }
+
+  public start(): void {
+    requestAnimationFrame(this.run);
+  }
+
+  private run = (time: number): void => {
+    const deltaTime = time - this.time;
+    this.time = time;
+    this.sceneManager.getCurrentScene().update?.(deltaTime);
+    requestAnimationFrame(this.run);
+  };
+
+  private createSceneManager(): SceneManager<SceneName> {
+    return new SceneManager<SceneName>(
+      {
+        [SceneName.Load]: new LoadScene(this),
+        [SceneName.Idle]: new IdleScene(this),
+        [SceneName.Play]: new PlayScene(this),
+        [SceneName.Over]: new OverScene(this),
+      },
+      SceneName.Load,
+    );
+  }
+}
